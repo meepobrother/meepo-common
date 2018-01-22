964c786f47b1ac13e23f0f4b9abbc5087ffd10be
15138730370 <1037483576@qq.com>
2018-01-22 15:13:43 +0800

    add rxjs

--- dist/bundles/meepo-common.umd.js
+++ dist/bundles/meepo-common.umd.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core')) :
-	typeof define === 'function' && define.amd ? define(['exports', '@angular/core'], factory) :
-	(factory((global['meepo-common'] = {}),global.ng.core));
-}(this, (function (exports,core) { 'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('rxjs/add/operator/debounce'), require('rxjs/add/operator/debounceTime'), require('rxjs/add/operator/distinctUntilChanged'), require('rxjs/add/operator/filter'), require('rxjs/add/operator/first'), require('rxjs/add/operator/ignoreElements'), require('rxjs/add/operator/last'), require('rxjs/add/operator/sample'), require('rxjs/add/operator/single'), require('rxjs/add/operator/skip'), require('rxjs/add/operator/skipUntil'), require('rxjs/add/operator/skipWhile'), require('rxjs/add/operator/take'), require('rxjs/add/operator/takeUntil'), require('rxjs/add/operator/takeWhile'), require('rxjs/add/operator/takeLast'), require('rxjs/add/operator/startWith'), require('rxjs/add/operator/throttle'), require('rxjs/add/operator/throttleTime'), require('rxjs/add/operator/map'), require('rxjs/add/operator/reduce'), require('rxjs/add/operator/scan'), require('rxjs/add/operator/concatMap'), require('rxjs/add/operator/switchMap'), require('rxjs/add/operator/combineAll'), require('rxjs/add/operator/withLatestFrom'), require('rxjs/add/operator/bufferCount'), require('rxjs/add/operator/switchMapTo'), require('rxjs/add/operator/every'), require('rxjs/add/operator/defaultIfEmpty'), require('rxjs/add/operator/do'), require('rxjs/observable/of'), require('rxjs/observable/empty'), require('rxjs/observable/from'), require('rxjs/observable/interval'), require('rxjs/observable/timer'), require('rxjs/observable/fromEvent'), require('rxjs/observable/fromPromise'), require('rxjs/observable/range'), require('rxjs/observable/fromEventPattern'), require('rxjs/observable/combineLatest'), require('rxjs/observable/concat'), require('rxjs/observable/forkJoin'), require('rxjs/observable/merge'), require('rxjs/observable/race'), require('rxjs/observable/zip'), require('rxjs/Scheduler'), require('rxjs/Subject'), require('rxjs/Observable')) :
+	typeof define === 'function' && define.amd ? define(['exports', '@angular/core', 'rxjs/add/operator/debounce', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/distinctUntilChanged', 'rxjs/add/operator/filter', 'rxjs/add/operator/first', 'rxjs/add/operator/ignoreElements', 'rxjs/add/operator/last', 'rxjs/add/operator/sample', 'rxjs/add/operator/single', 'rxjs/add/operator/skip', 'rxjs/add/operator/skipUntil', 'rxjs/add/operator/skipWhile', 'rxjs/add/operator/take', 'rxjs/add/operator/takeUntil', 'rxjs/add/operator/takeWhile', 'rxjs/add/operator/takeLast', 'rxjs/add/operator/startWith', 'rxjs/add/operator/throttle', 'rxjs/add/operator/throttleTime', 'rxjs/add/operator/map', 'rxjs/add/operator/reduce', 'rxjs/add/operator/scan', 'rxjs/add/operator/concatMap', 'rxjs/add/operator/switchMap', 'rxjs/add/operator/combineAll', 'rxjs/add/operator/withLatestFrom', 'rxjs/add/operator/bufferCount', 'rxjs/add/operator/switchMapTo', 'rxjs/add/operator/every', 'rxjs/add/operator/defaultIfEmpty', 'rxjs/add/operator/do', 'rxjs/observable/of', 'rxjs/observable/empty', 'rxjs/observable/from', 'rxjs/observable/interval', 'rxjs/observable/timer', 'rxjs/observable/fromEvent', 'rxjs/observable/fromPromise', 'rxjs/observable/range', 'rxjs/observable/fromEventPattern', 'rxjs/observable/combineLatest', 'rxjs/observable/concat', 'rxjs/observable/forkJoin', 'rxjs/observable/merge', 'rxjs/observable/race', 'rxjs/observable/zip', 'rxjs/Scheduler', 'rxjs/Subject', 'rxjs/Observable'], factory) :
+	(factory((global['meepo-common'] = {}),global.ng.core,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable.prototype,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable,global.Rx,global.Rx,global.Rx));
+}(this, (function (exports,core,debounce,debounceTime,distinctUntilChanged,filter,first,ignoreElements,last,sample,single,skip,skipUntil,skipWhile,take,takeUntil,takeWhile,takeLast,startWith,throttle,throttleTime,map,reduce,scan,concatMap,switchMap,combineAll,withLatestFrom,bufferCount,switchMapTo,every,defaultIfEmpty,_do,of,empty,from,interval,timer,fromEvent,fromPromise,range,fromEventPattern,combineLatest,concat,forkJoin,merge,race,zip,Scheduler,Subject,Observable) { 'use strict';
 
 /**
  * @fileoverview added by tsickle
@@ -148,6 +148,24 @@ MeepoCommonModule.ctorParameters = function () { return []; };
 exports.MeepoCommonModule = MeepoCommonModule;
 exports.ɵb = NgEachOf;
 exports.ɵa = NgEachOfContext;
+exports.of = of.of;
+exports.empty = empty.empty;
+exports.from = from.from;
+exports.interval = interval.interval;
+exports.timer = timer.timer;
+exports.fromEvent = fromEvent.fromEvent;
+exports.fromPromise = fromPromise.fromPromise;
+exports.range = range.range;
+exports.fromEventPattern = fromEventPattern.fromEventPattern;
+exports.combineLatest = combineLatest.combineLatest;
+exports.concat = concat.concat;
+exports.forkJoin = forkJoin.forkJoin;
+exports.merge = merge.merge;
+exports.race = race.race;
+exports.zip = zip.zip;
+exports.Scheduler = Scheduler.Scheduler;
+exports.Subject = Subject.Subject;
+exports.Observable = Observable.Observable;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
--- dist/esm5/meepo-common.js
+++ dist/esm5/meepo-common.js
@@ -1,4 +1,35 @@
 import { Directive, Input, KeyValueDiffers, NgModule, TemplateRef, ViewContainerRef } from '@angular/core';
+import 'rxjs/add/operator/debounce';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/ignoreElements';
+import 'rxjs/add/operator/last';
+import 'rxjs/add/operator/sample';
+import 'rxjs/add/operator/single';
+import 'rxjs/add/operator/skip';
+import 'rxjs/add/operator/skipUntil';
+import 'rxjs/add/operator/skipWhile';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/takeLast';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/throttle';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/reduce';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/concatMap';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/combineAll';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/bufferCount';
+import 'rxjs/add/operator/switchMapTo';
+import 'rxjs/add/operator/every';
+import 'rxjs/add/operator/defaultIfEmpty';
+import 'rxjs/add/operator/do';
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -148,7 +179,29 @@ MeepoCommonModule.ctorParameters = function () { return []; };
  * @suppress {checkTypes} checked by tsc
  */
 /**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
  * Generated bundle index. Do not edit.
  */
 export { MeepoCommonModule, NgEachOf as ɵb, NgEachOfContext as ɵa };
+export { of } from 'rxjs/observable/of';
+export { empty } from 'rxjs/observable/empty';
+export { from } from 'rxjs/observable/from';
+export { interval } from 'rxjs/observable/interval';
+export { timer } from 'rxjs/observable/timer';
+export { fromEvent } from 'rxjs/observable/fromEvent';
+export { fromPromise } from 'rxjs/observable/fromPromise';
+export { range } from 'rxjs/observable/range';
+export { fromEventPattern } from 'rxjs/observable/fromEventPattern';
+export { combineLatest } from 'rxjs/observable/combineLatest';
+export { concat } from 'rxjs/observable/concat';
+export { forkJoin } from 'rxjs/observable/forkJoin';
+export { merge } from 'rxjs/observable/merge';
+export { race } from 'rxjs/observable/race';
+export { zip } from 'rxjs/observable/zip';
+export { Scheduler } from 'rxjs/Scheduler';
+export { Subject } from 'rxjs/Subject';
+export { Observable } from 'rxjs/Observable';
 //# sourceMappingURL=meepo-common.js.map
